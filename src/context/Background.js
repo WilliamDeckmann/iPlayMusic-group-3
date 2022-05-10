0f7cfad95cd859930545424ac6eaa5dd9e8271d4
AndreasHjorth11 <77662628+AndreasHjorth11@users.noreply.github.com>
2022-05-10 11:15:03 +0200

    Add Basic Darkmode

---
+++ src/context/Background.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Background = ({ children }) => {
+  return <body className='bg-white dark:bg-BleachedCedar'>{children}</body>;
+};
+
+export default Background;
--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,13 +1,17 @@
 const Welcome = () => {
   return (
     <>
-      <div class='h-[768px] w-full bg-BleachedCedar'>
+      <div class='h-[768px] w-full dark:bg-BleachedCedar bg-white'>
         <div class='flex justify-center relative'>
           <img
             src='/assets/dark.png'
             class='w-[160px] small:mt-[70%] large:mt-[50%] tablet:mt-[30%] laptopS:mt-[25%] laptopL:mt-[17%] 4K:mt-[10%] absolute'
           ></img>
-          <h2 class='text-white text-[32px] font-Poppins font-bold small:mt-[130%] medium:mt-[120%] large:mt-[95%] tablet:mt-[55%] laptopS:mt-[42%] laptopL:mt-[30%] 4K:mt-[17%] absolute'>
+          <img
+            src='/assets/iplay.png'
+            class='w-[160px] small:mt-[70%] large:mt-[50%] tablet:mt-[30%] laptopS:mt-[25%] laptopL:mt-[17%] 4K:mt-[10%] absolute dark:hidden'
+          ></img>
+          <h2 class='text-black text-[32px] dark:text-white font-Poppins font-bold small:mt-[130%] medium:mt-[120%] large:mt-[95%] tablet:mt-[55%] laptopS:mt-[42%] laptopL:mt-[30%] 4K:mt-[17%] absolute'>
             iPlayMusic
           </h2>
         </div>
