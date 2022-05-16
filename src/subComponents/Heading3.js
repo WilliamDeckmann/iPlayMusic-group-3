dee8c42004e254cc23b1f192221323e43a128b78
CecileAnna <6263325@RTS-365.dk>
2022-05-16 13:30:15 +0200

    hardcodet login page

--- src/subComponents/Heading3.js
+++
@@ -1,9 +0,0 @@
-const Heading3 = (Heading) => {
-  return (
-    <>
-      <h3>{Heading}</h3>
-    </>
-  );
-};
-
-export default Heading3;
--- src/subComponents/LongBtn.js
+++ src/subComponents/LongBtn.js
@@ -1,7 +1,9 @@
-const LongBtn = (Text) => {
+const LongBtn = (props) => {
   return (
     <>
-      <button>{Text}</button>
+      <button className='rounded-full border-White border-2 uppercase text-White p-4 w-full my-16 text-sm font-bold tracking-widest'>
+        {props.text}
+      </button>
     </>
   );
 };
