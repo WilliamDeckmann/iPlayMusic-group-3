1ddbb692dc9d60981fd12ded5acb381351c022a1
WilliamDeckmann <u773560@rts-365.dk>
2022-05-20 11:21:17 +0200

    Update text

--- src/subComponents/HeadingBig.js
+++ src/subComponents/HeadingBig.js
@@ -1,6 +1,6 @@
 const HeadingBig = (props) => {
   return (
-    <h3 className="text-big font-Poppins" style={{color: props.color, fontWeight: props.weight}}>
+    <h3 className="text-big font-Poppins dark:text-white text-BleachedCedar" style={{color: props.color, fontWeight: props.weight}}>
       {props.text}
       
     </h3>
